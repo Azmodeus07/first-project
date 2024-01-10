d4a23f3be86de10a45d00b5a8d2ad441ed0cbf8f
Hritik Yadav <hritiksinghyadav02@gmail.com>
2024-01-10 11:46:21 +0530

    about

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Footer from './Components/Footer/Footer';
 import men_banner from './Components/Assets/banner_mens.png'
 import women_banner from './Components/Assets/banner_women.png'
 import kids_banner from './Components/Assets/banner_kids.png'
+import About from './Components/About/About';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route path="/product" element={<Product/>}>
           <Route path=':productId' element={<Product/>}/>
         </Route>  
+        <Route path='/About' element={<About/>}/>
         <Route path='/cart' element={<Cart/>}/>  
         <Route path='/login' element={<LoginSignup/>}/>  
       </Routes>   
